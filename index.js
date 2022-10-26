2734539eaa94a3d487c2ae0ed96786391187043d
mahian ahmad <mahianahamd9@gmail.com>
2022-10-26 12:42:36 +0600

    find specific id

--- index.js
+++ index.js
@@ -4,10 +4,15 @@ const cors = require('cors');
 const port = process.env.port || 5000;
 app.use(cors());
 
-const course = require('./data/course.json');
+const courses = require('./data/course.json');
 
-app.get('/course', (req, res) => {
-    res.send(course);
+app.get('/courses', (req, res) => {
+    res.send(courses);
+})
+app.get('/courses/:id', (req, res) => {
+    const id = req.params.id;
+    const specificId = courses.find(course => course.id === parseInt(id));
+    res.send(specificId);
 })
 
 app.listen(port, ()=>{
