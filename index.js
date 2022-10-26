22545099cdb42b539c17fe238f896ae3d24df347
mahian ahmad <mahianahamd9@gmail.com>
2022-10-26 17:21:58 +0600

    add catagory select

--- index.js
+++ index.js
@@ -5,10 +5,26 @@ const port = process.env.port || 5000;
 app.use(cors());
 
 const courses = require('./data/course.json');
+const categories = require('./data/categories.json');
 
 app.get('/courses', (req, res) => {
     res.send(courses);
 })
+
+app.get('/category', (req, res) => {
+    res.send(categories);
+})
+
+app.get('/category/:id', (req, res) => {
+    const id = req.params.id;
+    const specificCategory = courses.filter(course => course.categoryId === parseInt(id))
+    if(id == 0){
+        res.send(courses)
+    }else{
+        res.send(specificCategory);
+    }
+})
+
 app.get('/courses/:id', (req, res) => {
     const id = req.params.id;
     const specificId = courses.find(course => course.id === parseInt(id));
